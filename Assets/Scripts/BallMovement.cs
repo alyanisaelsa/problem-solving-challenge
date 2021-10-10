ef05acc652fd141efacdc8025b6478c034f27250
Alyanisa Elsa Ayu Anandia <90304203+alyanisaelsa@users.noreply.github.com>
2021-10-10 17:28:03 +0700

    Add files via upload

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed1;
+    public float speed2;
+    Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(new Vector2(speed1, speed2));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
