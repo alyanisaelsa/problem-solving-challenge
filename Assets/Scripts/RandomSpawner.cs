b78fdd2cfd3925dca65a0009d8de60c1e19f75f8
Alyanisa Elsa Ayu Anandia <90304203+alyanisaelsa@users.noreply.github.com>
2021-10-10 21:14:42 +0700

    Add files via upload

---
+++ Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject prefab1;
+    public float SpawnRate = 0f;
+    public float nextSpawn = 0f;
+    float randomX;
+    int randomY;
+    private float[] linepos = { 3.84f, 2.43f, 2.12f, 1.52f, -3.29f};
+    Vector2 whereToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            randomX = Random.Range (1f, 6f);
+            randomY = Random.Range(0, linepos.Length);
+            whereToSpawn = new Vector2(randomX, linepos[randomY]);
+            Instantiate(prefab1, whereToSpawn, Quaternion.identity);
+            
+
+            nextSpawn = Time.time + SpawnRate;
+            Debug.Log(Time.time + SpawnRate);
+        }
+    }
+}
