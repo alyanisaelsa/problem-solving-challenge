2b088205c188ed5d6b2072ef483797ffdd7cd109
Alyanisa Elsa Ayu Anandia <90304203+alyanisaelsa@users.noreply.github.com>
2021-10-12 12:23:25 +0700

    Add files via upload

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rigidbody;
+    private Vector2 moveDirection;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal"); //a,d kiri kanan
+        float moveY = Input.GetAxis("Vertical"); //w,s atas bawah
+
+        moveDirection.Set(moveX, moveY);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = moveDirection.normalized * speed;
+    }
+}
